03708740ffa7e343dc0e4816aa72ebd316253233
Minimage <seancampbell914@gmail.com>
2022-05-06 15:26:53 +0000

    pushing

--- src/js/views/Characters.js
+++ src/js/views/Characters.js
@@ -15,23 +15,33 @@ export const Characters = () => {
   }, []);
 
     return(
+
+
         <div>
+          {console.log(characters)}
             <ul>
         {characters.map((item, index) => {
           let link;
-            switch (item.name) {
-              case 'Luke Skywalker':
-                link = 'https://media.vanityfair.com/photos/56dafc6130c1a42b3fbdafb8/1:1/w_719,h_719,c_limit/skywalker-luke.0.0.jpg';
-                default:
-                  link = 'https://bitsofco.de/content/images/2018/12/broken-1.png';
-                  break;
-              
+          let links = []
+            // switch (item.name) {
+            //   case 'Luke Skywalker':
+            //     link = 'https://media.vanityfair.com/photos/56dafc6130c1a42b3fbdafb8/1:1/w_719,h_719,c_limit/skywalker-luke.0.0.jpg';
+            //     default:
+            //       link = 'https://bitsofco.de/content/images/2018/12/broken-1.png';
+            //       break;
+
+            item.name == 'Luke Skywalker' ? links.push('https://media.vanityfair.com/photos/56dafc6130c1a42b3fbdafb8/1:1/w_719,h_719,c_limit/skywalker-luke.0.0.jpg') : links.push('https://bitsofco.de/content/images/2018/12/broken-1.png');
+            item.name == 'C-3PO' ? links.push('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS16WXnMgCQgy47gxUukZwvL3vubz8EczbD1w&usqp=CAU') : links.push('https://bitsofco.de/content/images/2018/12/broken-1.png');
+            console.log(characters.includes(links[index]))
+
+            
+            
 
         return (
 
            
 <div className="card" style={{width: "18rem;"}}>
-<img class="card-img-top" src={link} alt="Card image cap"/>
+<img class="card-img-top" src={links[index]} alt="Card image cap"/>
 <div className="card-body">
   <h5 className="card-title"> {item.name}</h5>
   <p className="card-text">Gender: {item.gender}</p>
@@ -40,7 +50,7 @@ export const Characters = () => {
   <a href="#" className="btn btn-primary">Go somewhere</a>
 </div>
 </div>
-            )};
+            )
         })}
       </ul>
 
